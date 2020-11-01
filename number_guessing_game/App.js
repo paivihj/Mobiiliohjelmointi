49c686d88a99d22cfe542247e3e265985f0f5c5f
Päivi <paivi.julku@myy.haaga-helia.fi>
2020-11-01 17:02:58 +0200

    Number guessing game

---
+++ number_guessing_game/App.js
@@ -0,0 +1,83 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
+import { StyleSheet, Alert, TextInput, Button, Text, View } from 'react-native';
+
+var number = Math.floor(Math .random() * 100) + 1;
+var times = -1;
+
+export default function App() {
+
+  const [value, setValue] = useState(0);
+  const [guess, setGuess] = useState(0);
+
+if (guess==value){
+  times++;
+}
+
+if (guess == 0){
+  return (<View style={styles.container}>
+    Guess a number between 1-100
+    <TextInput style={styles.input}
+      keyboardType = 'numeric'
+      value={value}
+      onChangeText={value => setValue(value)}
+    />
+    <Button 
+      onPress={guess => setGuess(value)}
+      title="Make a Guess!"
+      />
+  </View>)
+}
+
+if (guess > number){
+  return(<View style={styles.container}>
+      Your guess {guess} is too high
+      <TextInput style={styles.input}
+      keyboardType = 'numeric'
+      value={value}
+      onChangeText={value => setValue(value)}
+    />
+    <Button 
+      onPress={guess => setGuess(value)}
+      title="Make a Guess!"
+      />
+    </View>)
+}
+
+if (guess < number){
+  return(  
+    <View style={styles.container}>
+    Your guess {guess} is too low
+    <TextInput style={styles.input}
+      keyboardType = 'numeric'
+      value={value}
+      onChangeText={value => setValue(value)}
+    />
+    <Button 
+      onPress={guess => setGuess(value)}
+      title="Make a Guess!"
+      />
+  </View>
+  )
+}
+else if (guess==number){
+  return(Alert.alert("You guessed the number " + number + " in " + times + " guesses"))
+}}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    fontFamily: 'Arial',
+  },
+   input:{
+    margin:20,
+    padding: 5,
+    width: 40,
+    borderWidth: 0.5,
+    borderRadius: 4,
+    backgroundColor: "#fff"
+   },
+});
