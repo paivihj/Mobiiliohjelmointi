a0dcd6ab35cc9fa4d3a0f9075aaa8305709b1c19
Päivi <paivi.julku@myy.haaga-helia.fi>
2020-11-01 17:30:21 +0200

    Fix Calculator

--- Calculator/App.js
+++ Calculator/App.js
@@ -1,21 +1,59 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Keyboard, TextInput, StyleSheet, Text, View, Button } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default function App() {		
+	
+	const [result, setResult]=useState(0);
+	const [firstNumber, setFirstNumber]=useState(0);
+	const [secondNumber, setSecondNumber]=useState(0);
+			
+			return(
+			<View style={styles.container}>
+			<div>Result: {result}</div>
+			<TextInput style={styles.input}
+				keyboardType = 'numeric'
+				value={firstNumber}
+				onChangeText={firstNumber => setFirstNumber(firstNumber)}
+			/>
+			<TextInput style={styles.input}
+				keyboardType = 'numeric'
+				value={secondNumber}
+				onChangeText={secondNumber => setSecondNumber(secondNumber)}
+			/>
+			<View style={styles.button}>
+			<Button 
+				onPress={ result => setResult(parseInt(firstNumber)+parseInt(secondNumber)) }
+				title="+"
+			/>
+			<Button
+				onPress={ result => setResult(firstNumber-secondNumber) }
+				title="-"
+			/></View>
+			</View>
+			);
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+	flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input:{
+    margin:3,
+    padding: 10,
+    borderWidth: 0.5,
+    borderRadius: 4,
+    backgroundColor: "#fff"
+   },
+   button:{
+	margin: 0,
+    flex: 2,
+    flexDirection: 'row',
+    alignItems: 'flex-start',
+   }
 });
+
+
